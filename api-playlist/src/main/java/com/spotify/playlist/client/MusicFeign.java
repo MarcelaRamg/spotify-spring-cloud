5e5edea749c744b38708331394972415184e0134
Marcela Ramirez <marcela.ramg@gmail.com>
2022-11-09 15:47:14 -0500

    Update

--- api-playlist/src/main/java/com/spotify/playlist/client/MusicFeign.java
+++ api-playlist/src/main/java/com/spotify/playlist/client/MusicFeign.java
@@ -2,6 +2,7 @@ package com.spotify.playlist.client;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
